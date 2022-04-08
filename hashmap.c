d60270cda7f35b8407e4a79652ce0c886bd6093a
OscarRojasG <oscar.rojas.g@mail.pucv.cl>
2022-04-07 23:07:28 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -47,8 +47,7 @@ void insertMap(HashMap * map, char * key, void * value) {
             pos = 0;
         }
     }
-    strcpy(map->buckets[pos]->key, key);
-    map->buckets[pos]->value = value;
+    map->buckets[pos] = createPair(key, value);
     map->size++;
 }
 
