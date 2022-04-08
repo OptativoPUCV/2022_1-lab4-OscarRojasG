85aead0b2200f548f0543ebd9e3869923129c8e7
OscarRojasG1 <>
2022-04-08 15:28:48 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -101,6 +101,7 @@ Pair * firstMap(HashMap * map) {
 
 Pair * nextMap(HashMap * map) {
     if(map->size == 0) return NULL;
+    map->current++;
     while(map->buckets[map->current] == NULL || map->buckets[map->current]->key == NULL) {
         map->current++;
     }
